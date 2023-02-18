d0144b3c03b4fb38ca60ad80106e2a27db71aff6
Soham Padia <sohampadia10@gmail.com>
2023-02-18 11:58:35 +0530

    d

---
+++ android/app/src/main/kotlin/com/example/ibreathe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ibreathe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:ibreathe/setSplashScreen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'account_page.dart';
+import 'login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://saevexsfyqgxatepzklg.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZXZleHNmeXFneGF0ZXB6a2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY2OTkzMzUsImV4cCI6MTk5MjI3NTMzNX0.v4CPFq8qjqkDmQoCte489pDDPEXrRc_Rg0nay4Yynsk',
+  );
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Supabase Flutter',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.green,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.green,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.green,
+          ),
+        ),
+      ),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (_) => const SplashPage(),
+        '/login': (_) => const LoginPage(),
+        '/account': (_) => const AccountPage(),
+      },
+    );
+  }
+}
